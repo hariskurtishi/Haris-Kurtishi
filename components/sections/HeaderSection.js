93521d1e46ab092371861dda264a5c455e73f4c0
hariskurtishi <Hariskay95@gmail.com>
2022-10-21 09:58:55 +0200

    changed global font size

--- components/sections/HeaderSection.js
+++ components/sections/HeaderSection.js
@@ -12,16 +12,16 @@ export default function HeaderSection() {
           <div className="md:col-6 ">
             <div className="relative">
               <div>
-                <h1 className="text-mirage-60   text-center md:text-left mb-4 md:mb-0">
+                <h1 className="text-mygreen-100   text-center md:text-left mb-4 md:mb-0">
                   Hello there! I’m <br />
-                  <span className="text-mirage-60  leading-normal text-xl text-center md:text-left lg:text-3xl font-bold ">
+                  <span className="text-blue-950  leading-normal text-xl text-center md:text-left lg:text-3xl">
                     Haris Kurtishi
                   </span>
                 </h1>
               </div>
 
               <div className="lg:py-0 text-mirage-60 ">
-                <h1 className="text-[55px] lg:text-[100px] lg:-ml-[5px] text-center md:text-left font-semibold lg:leading-tight leading-8 md:leading-tight">
+                <h1 className="text-[55px] lg:text-[100px] lg:-ml-[5px] text-center md:text-left font-semibold lg:leading-tight leading-8 md:leading-tight ">
                   Frontend
                 </h1>
                 <h1 className="text-[55px] lg:text-[100px] font-semibold lg:ml-[49px] md:text-left text-center lg:leading-[70px] leading-snug">
@@ -63,7 +63,7 @@ export default function HeaderSection() {
 
           <div className="col-12 relative md:col-5 md:offset-1 md:flex md:justify-center pt-24 md:pt-14 lg:mt-24">
             {/* <div
-              className="flex absolute justify-center shadow-xl   top-48 md:-top-0 transition-all left-0  p-2 bg-zinc-100"
+              className="flex absolute justify-center shadow-xl   top-48 md:-top-0 transition-all left-0  p-2 bg-mirage-60"
               style={{ zIndex: '45' }}
             >
               <div className="flex gap-2">
@@ -74,35 +74,35 @@ export default function HeaderSection() {
                     alt="reddot"
                   />
                 </div>
-                <p className="text-sm md:text-base text-zinc-800 flex items-center">Typescript</p>
+                <p className="text-sm md:text-base text-zinc-900 flex items-center">Typescript</p>
               </div>
             </div> */}
             <Fade
-              className="flex absolute justify-center shadow-xl transition-all md:top-64  top-96 lg:top-[155px] left-6 lg:left-14  p-2 bg-zinc-100"
+              className="flex absolute justify-center shadow-xl transition-all md:top-64  top-96 lg:top-[155px] left-6 lg:left-14  p-2 bg-mirage-60"
               style={{ zIndex: '45' }}
             >
               <div className="flex gap-2">
                 <div className="flex justify-center">
                   <SVG src="/svg/nextjs.svg" className="w-6 h-6" alt="reddot" />
                 </div>
-                <p className="text-sm md:text-base text-zinc-800 flex items-center">Next Js</p>
+                <p className="text-sm md:text-base text-zinc-900 flex items-center">Next Js</p>
               </div>
             </Fade>
 
             <Fade
-              className="flex absolute justify-center shadow-xl transition-all right-2 md:right-18 lg:right-10 top-[480px] md:top-[440px] lg:top-72 p-2 bg-zinc-100"
+              className="flex absolute justify-center shadow-xl transition-all right-2 md:right-18 lg:right-10 top-[480px] md:top-[440px] lg:top-72 p-2 bg-mirage-60"
               style={{ zIndex: '999' }}
             >
               <div className="flex gap-2">
                 <div className="flex justify-center">
                   <SVG src="/svg/react.svg" className="w-6 h-6  fill-current" alt="reddot" />
                 </div>
-                <p className="text-sm md:text-base text-zinc-800 flex items-center">React Js</p>
+                <p className="text-sm md:text-base text-zinc-900 flex items-center">React Js</p>
               </div>
             </Fade>
 
             <Fade
-              className="flex absolute justify-center shadow-xl transition-all right-4 top-64 md:top-0 lg:top-0 md:right-18 lg:right-8  p-2 bg-zinc-100"
+              className="flex absolute justify-center shadow-xl transition-all right-4 top-64 md:top-0 lg:top-0 md:right-18 lg:right-8  p-2 bg-mirage-60"
               style={{ zIndex: '45' }}
             >
               <div className="flex gap-2">
@@ -113,7 +113,7 @@ export default function HeaderSection() {
                     alt="reddot"
                   />
                 </div>
-                <p className="text-sm md:text-base text-zinc-800 flex items-center">Tailwind Css</p>
+                <p className="text-sm md:text-base text-zinc-900 flex items-center">Tailwind Css</p>
               </div>
             </Fade>
 
